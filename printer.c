19c763e53c76512a278ddc622587b6cce92135bf
Mikolaj <mikolajdsp10@gmail.com>
2023-05-19 20:15:41 +0200

    Initial Commit

--- printer.c
+++ printer.c
@@ -3,9 +3,10 @@ struct Printer{
     
 };
 Printer* printer_new(void){
-
+    Printer* p = calloc(1,sizeof(Printer));
+    return p;
 }
 
 void printer_delete(Printer* printer){
-
+    free(printer);
 }
